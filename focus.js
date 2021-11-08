70e6f73454826002d41086ad0809bdff1a703656
dhruv-solanki <dhruvsolanki34@gmail.com>
2021-11-08 17:16:58 +0530

    Quote API integrated for random quote

--- focus.js
+++ focus.js
@@ -3,15 +3,31 @@ var dateElement = document.getElementById('date');
 
 var bgImageElement = document.getElementById('unsplash-img');
 
+var quoteElement = document.getElementById('quote');
+var authorElement = document.getElementById('author');
+
 getBackgroundImage();
 
 async function getBackgroundImage() {
-    var reponse = await fetch('https://source.unsplash.com/random/1600x900');
-    var imageURL = reponse.url;
+    var response = await fetch('https://source.unsplash.com/random/1600x900');
+    var imageURL = response.url;
 
     bgImageElement.style.backgroundImage = "url('" + imageURL + "')";
     bgImageElement.style.backgroundRepeat = "no-repeat";
     bgImageElement.style.backgroundSize = "cover";
+}
+
+getQuote();
+
+async function getQuote() {
+    var response = await fetch('https://api.quotable.io/random');
+    var json = await response.json();
+
+    var quote = json.content;
+    var author = json.author;
+
+    quoteElement.innerText = quote;
+    authorElement.innerText = author; 
 }
 
 setInterval(getTime, 1000);
