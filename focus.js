4daf971306bd29624c5584a2820f9be2a9803bfb
Chirag Ramchandanee <chiragramchandanee@outlook.com>
2022-10-15 16:29:58 +0530

    dev-rchirag101: code formatting

--- focus.js
+++ focus.js
@@ -6,47 +6,47 @@ var bgImageElement = document.getElementById("unsplash-img");
 var quoteElement = document.getElementById("quote");
 var authorElement = document.getElementById("author");
 
-async function getBackgroundImage() {
-	var response = await fetch("https://source.unsplash.com/random/1600x900");
-	var imageURL = response.url;
+const getBackgroundImage = async () => {
+    var response = await fetch("https://source.unsplash.com/random/1600x900");
+    var imageURL = response.url;
 
-	bgImageElement.style.backgroundImage = `url(${imageURL})`;
-	bgImageElement.style.backgroundRepeat = "no-repeat";
-	bgImageElement.style.backgroundSize = "cover";
-}
+    bgImageElement.style.backgroundImage = `url(${imageURL})`;
+    bgImageElement.style.backgroundRepeat = "no-repeat";
+    bgImageElement.style.backgroundSize = "cover";
+};
 
-async function getQuote() {
-	var response = await fetch("https://api.quotable.io/random");
-	var json = await response.json();
+const getQuote = async () => {
+    var response = await fetch("https://api.quotable.io/random");
+    var json = await response.json();
 
-	var quote = json.content;
-	var author = json.author;
+    var quote = json.content;
+    var author = json.author;
 
-	quoteElement.innerText = quote;
-	authorElement.innerText = author;
-}
+    quoteElement.innerText = quote;
+    authorElement.innerText = author;
+};
 
-function getTime() {
-	var currentTime = new Date();
+const getTime = () => {
+    var currentTime = new Date();
 
-	var showDate = currentTime.toLocaleDateString("en-US", {
-		day: "numeric",
-		month: "short",
-		year: "numeric",
-	});
+    var showDate = currentTime.toLocaleDateString("en-US", {
+        day: "numeric",
+        month: "short",
+        year: "numeric",
+    });
 
-	var showTime = currentTime.toLocaleString("en-US", {
-		hour: "numeric",
-		minute: "numeric",
-		second: "numeric",
-		hour12: true,
-	});
+    var showTime = currentTime.toLocaleString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        hour12: true,
+    });
 
-	dateElement.innerText = showDate.toString();
-	timeElement.innerText = showTime.toString();
+    dateElement.innerText = showDate.toString();
+    timeElement.innerText = showTime.toString();
 
-	setInterval(getTime, 1000);
-}
+    setInterval(getTime, 1000);
+};
 
 getBackgroundImage();
 getQuote();
